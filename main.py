0e8afd1177e0b5efe71d44b6cbfc26ea6832775c
J20Man <josephttwentyman@hotmail.co.uk>
2025-09-28 22:18:03 +0100

    Fixed the stupid fucking function

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 def main():
     from stats import count_words
     from stats import char_count
+    from stats import makes_list
+    from stats import dict_sort
+    #from stats import sorted_text
     file = "books/frankenstein.txt"
-    
-    print(char_count(get_book_text(file)))
+    text = get_book_text(file)
+
+    total_chars = count_words(text)
+    char_count_dict = char_count(text)
+    new_list = makes_list(char_count_dict)
+    sorted_dict = dict_sort(new_list)
+
+    #print(sorted_dict)
+    print(f"============ BOOKBOT ============")
+    print("Analyzing book found at {file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_chars} total words")
+    print("--------- Character Count -------")
+    print(sorted_dict)
 
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-
-
 
 
 
--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
 def count_words(bk_text):
+    #this counts the amount of letters there are total
     text = bk_text.split()
     count = 0
 
@@ -7,6 +8,7 @@ def count_words(bk_text):
     return count
 
 def char_count(bk_text):
+    #this counts how many of each char there is and adds it into a dictionary
     chars = {}
     for c in bk_text:
         lower_c = c.lower()
@@ -15,3 +17,17 @@ def char_count(bk_text):
         else:
             chars[lower_c] = 1
     return chars
+
+def makes_list(book_txt):
+    to_sort = []
+    for ch, count in book_txt.items():
+        if ch.isalpha():
+            to_sort.append({"char": ch, "num": count})
+    return to_sort
+    
+def sort_on(item):
+    return item["num"]
+    
+def dict_sort(to_sort):
+    to_sort.sort(reverse=True, key=sort_on)
+    return to_sort
